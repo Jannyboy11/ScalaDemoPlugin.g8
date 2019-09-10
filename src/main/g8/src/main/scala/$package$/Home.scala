d262f07eb86c76bf6738e270ebf78a8c9a21ac6e
Jan Boerman <Janboerman95@gmail.com>
2019-09-10 16:57:47 +0200

    update to latests scala, minecraft, scalaloader

--- src/main/g8/src/main/scala/$package$/Home.scala
+++ src/main/g8/src/main/scala/$package$/Home.scala
@@ -6,7 +6,7 @@ import org.bukkit.Location
 
 object Home {
     def apply(owner: UUID, name: String, location: Location): Home = new Home(owner, name, location.clone())
-    def unapply(arg: Home): Option[(UUID, String, Location)] = Some(arg.owner, arg.name, arg.location.clone())
+    def unapply(arg: Home): Option[(UUID, String, Location)] = Some(arg.getOwner(), arg.getName(), arg.getLocation())
 }
 
 class Home private(private val owner: UUID, private val name: String, private val location: Location) {
